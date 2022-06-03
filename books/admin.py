89ef86d2932707e129122f9d7b00f958a0885401
MeshkovDI_PC <rdm.dima@mail.ru>
2022-06-04 03:44:05 +0700

    Added models Books, Author, Category

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book, Category, Author
+
+admin.site.register(Book)
+
+admin.site.register(Category)
+
+admin.site.register(Author)
--- books/models.py
+++ books/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+
